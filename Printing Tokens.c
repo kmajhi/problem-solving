8449f68a60cf01524143be57ff5dd4bd95beaf39
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2024-07-03 00:29:05 +0600

    Update Printing Tokens.c

--- Printing Tokens.c
+++ Printing Tokens.c
@@ -4,11 +4,15 @@
 #include <stdlib.h>
 
 int main() {
-
     char *s;
     s = malloc(1024 * sizeof(char));
     scanf("%[^\n]", s);
     s = realloc(s, strlen(s) + 1);
-    //Write your logic to print the tokens of the sentence here.
+    for(int i=0; s[i]!= NULL; i++){
+        if(s[i] == ' '){
+            s[i] = '\n';
+        }
+    }
+    printf("%s", s);
     return 0;
 }
